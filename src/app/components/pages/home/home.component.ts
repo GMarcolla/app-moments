691105014a1ca7f512085ad23855278112311d60
GMarcolla <gustavomarcolla123@gmail.com>
2025-02-20 18:48:58 -0300

    Desenvolve a pesquisa entre os momentos

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -17,6 +17,9 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
+  faSearch = faSearch;
+  searchTerm = '';
+
   ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
       const data = items.data;
@@ -29,5 +32,14 @@ export class HomeComponent implements OnInit {
       this.allMoments = data;
       this.moments = data;
     });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value.toLowerCase())
+    );
   }
 }
